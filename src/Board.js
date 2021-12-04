bc9bd9544dd0a31937ce40f872fee0b2ff1b13b3
michael_k <michael_kalamogan@emf.com.sg>
2021-12-04 12:30:42 +0800

    change cell colors

--- src/Board.js
+++ src/Board.js
@@ -7,7 +7,10 @@ class Board extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {gameBoard: this.newBoard()}
+    this.state = {
+      gameBoard: this.newBoard(),
+      gameStatus: "inProgress"
+    }
     this.randomStatus = this.randomStatus.bind(this)
   }
 
@@ -28,25 +31,77 @@ class Board extends Component {
         }
       
     }
+
+    console.log(board)
     return board
   }
 
+  updateBoard (row, col) {
+    console.log(row,col)
+    function changeStatus (Board, row, col) {
+      if(Board[row][col] === 'F') {
+        Board[row][col] = 'T'
+       
+      } else {
+        Board[row][col] = 'F'
+      }
+      return Board
+    }
+
+    let Board = this.state.gameBoard
+    changeStatus(Board, row, col)
+
+    if(row !== 4 && row !== 0) {
+      changeStatus(Board, row-1, col)
+      changeStatus(Board,row+1,col)      
+    } else if (row === 0) {
+      changeStatus(Board,row+1,col)
+    } else if (row === 4) {
+      changeStatus(Board,row-1, col)
+    }
+
+    if(col !== 0 && col !== 4) {
+      changeStatus(Board,row, col-1)
+      changeStatus(Board,row, col+1)
+    } else if (col===0) {
+      changeStatus(Board,row, col+1)
+    } else if (col===4) {
+      changeStatus(Board,row, col-1)
+    } 
+
+    this.setState({gameBoard: Board})
+
+  }
 
   render() {
 
     return (
       <div className="Board">
-        <table>
-          <tbody>
-            {this.state.gameBoard.map((row,i) => (
-              <tr key={i}>
-                {row.map((col,j) => (
-                  <td key={j}><Cell cellStatus={this.state.gameBoard[i][j]} /></td>
-                ))}
-              </tr>
-            ))}        
-          </tbody>
-        </table>
+        {this.state.gameStatus==="win" ? 
+          <div>
+            You Win
+          </div>
+        : 
+          <div>     
+            <table>
+              <tbody>
+                {this.state.gameBoard.map((row,i) => (
+                  <tr key={i}>
+                    {row.map((col,j) => (
+                      <td key={j} onClick={() => this.updateBoard(i,j)}><Cell cellStatus={this.state.gameBoard[i][j]} /></td>
+                    ))}
+                  </tr>
+                ))}        
+              </tbody>
+            </table>
+            <div className="instructions">
+              <h3>Objective of the game is to off all the Lights. You win when you off all the Lights.</h3>         
+              <p>White means that the Light is on. Grey means that the Light is off.</p>
+              
+              <p>Each time u toggle a light, the adjacent 4 lights will be switched on or off accordingly.</p>
+            </div>
+          </div>
+        }
       </div>
     );
   }
